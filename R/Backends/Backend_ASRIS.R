4156bceb9641aa929274d3dfa4149252593a7df8
RossDSearle <ross.searle@csiro.au>
2019-08-06 13:13:45 +1000

    sync

--- R/Backends/Backend_ASRIS.R
+++ R/Backends/Backend_ASRIS.R
@@ -4,26 +4,10 @@ library(RCurl)
 library(data.table)
 library(stringr)
 
-####     This needs to be finalised once Ramneek has finished building the endpoint   ########################
-
-# df <- fromJSON("http://kharak-bm/WebApplication1/api/LabResults")
-# write.csv(df, 'c:/temp/Asris.csv')
-#
-# jsn <- getURL("http://kharak-bm/WebApplication1/api/LabResults")
-# df <- fromJSON(jsn)
-#
-# write.csv(df, 'c:/temp/Asris.csv')
-# cat(jsn, file='c:/temp/Asris.json')
-#
-# df <- read.csv( 'c:/temp/Asris.csv')
-
-
-#df <- fromJSON('http://kharak-bm:53568/api/LabResults?method_code=15_NR_CA')
-
 ep <- 'http://asris-daas02/WebApplication1_deploy/api'
 
 
-getData_ASRIS<- function( observedProperty=NULL, observedPropertyGroup=NULL ){
+getData_ASRIS<- function(provider=NULL, observedProperty=NULL, observedPropertyGroup=NULL ){
 
   OrgName <- 'ASRIS'
 
@@ -45,13 +29,13 @@ getData_ASRIS<- function( observedProperty=NULL, observedPropertyGroup=NULL ){
 
       propType <- getPropertyType(prop)
 
-      units <- getUnits(propertyType = propertyType, prop = prop)
+      units <- getUnits(propertyType = propType, prop = prop)
 
       day <- str_sub(fdf$o_date_desc, 1,2)
       mnth <- str_sub(fdf$o_date_desc, 3,4)
       yr <- str_sub(fdf$o_date_desc, 5,8)
 
-      oOutDF <- generateResponseDF(OrgName, 'ASRIS', paste0(fdf$agency_code, '_', fdf$proj_code, '_', fdf$s_id, '_', fdf$o_id), fdf$samp_no ,paste0(day, '-', mnth, '-', yr,'T00:00:00' ) , fdf$o_longitude_GDA94, fdf$o_latitude_GDA94 ,
+      oOutDF <- generateResponseDF(provider, OrgName, paste0(fdf$agency_code, '_', fdf$proj_code, '_', fdf$s_id, '_', fdf$o_id), fdf$samp_no ,paste0(day, '-', mnth, '-', yr,'T00:00:00' ) , fdf$o_longitude_GDA94, fdf$o_latitude_GDA94 ,
                                    fdf$samp_upper_depth , fdf$samp_lower_depth , propType, prop, fdf$labr_value , units, 'Brilliant')
       lodfs[[i]] <- oOutDF
     }else{
