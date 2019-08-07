212dc2a5cd0a3dca9cd71345bec39868ecb2de85
Ross Searle <sea084@soils-discovery.it.csiro.au>
2019-08-07 14:06:03 +1000

    sync

---
+++ R/Docs/SoilDataFederatorR_Examples.R
@@ -0,0 +1,59 @@
+library(jsonlite)
+library(httr)
+library(htmltidy)
+library(XML)
+library(xml2)
+devtools::install_github("hrbrmstr/jsonview")
+library(jsonview)
+
+
+# Get the raw json from the Providers endpoint
+rawJson <- content(GET('http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/Providers'), "text")
+
+# View the raw json data
+json_view(rawJson)
+json_tree_view(rawJson)
+
+# Get the raw json from the Providers endpoint and create a dataframe - This is probably the best way to use the SoilDataFederator endpoint in R
+df <- fromJSON('http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/Providers')
+
+
+# Get the Providers data as XML
+rawXML <- content(GET('http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/Providers?format=xml'), "text")
+
+xml_view(rawXML)
+
+# Get the Providers data as XML and create a dataframe
+doc<-xmlParse(rawXML)
+xmldf <- xmlToDataFrame(nodes = getNodeSet(doc, "//DataProvider"))
+
+
+# Get the Providers data as a CSV file
+csvf <- tempfile()
+download.file('http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/Providers?format=csv', csvf, mode="wb", quiet = T)
+df <- read.csv(csvf)
+head(df)
+unlink(csvf)
+
+
+
+
+# get the soil property groups from the PropertyGroups endpoint
+url <- 'http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/Properties'
+df <- fromJSON(url)
+head(df, 20)
+
+
+
+# get the soil property groups from the PropertyGroups endpoint
+url <- 'http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/PropertyGroups'
+df <- fromJSON(url)
+head(df)
+
+
+
+
+# get the soil property data from the SoilData endpoint
+url <- 'http://esoil.io/TERNLandscapes/SoilDataFederatoR/R/SoilDataAPI/SoilData?observedProperty=3A1&providers=TasGovernment'
+df <- fromJSON(url)
+head(df)
