68c47a634a7209729d3e17ccda58fb2c2b17c38a
RossDSearle <ross.searle@csiro.au>
2019-08-20 09:10:31 +1000

    register code

--- R/Helpers/Register.R
+++ R/Helpers/Register.R
@@ -1,7 +1,20 @@
+library(DBI)
+library(RSQLite)
+
+machineName <- as.character(Sys.info()['nodename'])
+
+if(machineName=='soils-discovery'){
+  dbPathSoilsFed <- '/srv/plumber/TERNLandscapes/SoilDataFederatoR/DB/soilsFederator.sqlite'
+}else{
+  dbPathSoilsFed <- system.file("extdata", "soilsFederator.sqlite", package = "SoilDataFederatoR")
+}
 
 
 
+generateAPIKey <- function(){
   sql <- paste0("Select * from Providers where Active = 1 and Availability = 'Public'")
   #
   orgs = doQueryFromFed(sql)
-  return(orgs)
+  return("An email has been sent to you. Please click on the link to confirm your registration")
+
+}
