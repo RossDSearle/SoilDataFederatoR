06aaa1c670667d4b910df79eef440cfc89c333a4
Ross Searle <sea084@soils-discovery.it.csiro.au>
2020-08-26 12:21:19 +1000

    fixed api start services

--- R/WebAPI/StartServices.R
+++ R/WebAPI/StartServices.R
@@ -6,12 +6,15 @@ machineName <- as.character(Sys.info()['nodename'])
 if(machineName=='soils-discovery'){
 
   deployDir <-'/srv/plumber/TERNLandscapes/SoilDataFederatoR/R'
-  #server <- 'http://esoil.io'
   server <- '0.0.0.0'
-  #options("plumber.host" = "0.0.0.0")
-  #options("plumber.apiHost" = "0.0.0.0")
+
+  options("plumber.apiScheme" = "https")
+  options("plumber.apiPath" ='/TERNLandscapes/SoilDataFederatoR/R')
+  options("plumber.apiHost" = 'esoil.io')
+  options("plumber.apiURL" = 'https://esoil.io')
   portNum <- 8074
   #portNum <- 8073
+  print('######################  I am here   ############################')
 }else{
   deployDir <-'C:/Users/sea084/Dropbox/RossRCode/Git/TernLandscapes/APIs/SoilDataFederatoR/R'
   server <- '127.0.0.1'
@@ -29,13 +32,14 @@ source(paste0(deployDir, '/Backends.R'))
 
 r <- plumb(paste0(deployDir, "/WebAPI/apiEndPoints.R"))
 
-#server <- 'http://esoil.io'
-#portNum <- 8079
+#server <- 'esoil.io'
+#portNum <- 8074
 
 print(r)
 
-
+print(server)
 r$run(host= server, port=portNum, swagger=TRUE)
+
 
 
 
