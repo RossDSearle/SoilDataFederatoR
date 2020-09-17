38f81236178d51c2232a7dc19f9e700a0fdadf5e
Searle <sea084@csiro.au>
2020-09-18 09:42:34 +1000

    Implementing TERN Theme for web Page

--- Pages/ThemeTest/app.R
+++ Pages/ThemeTest/app.R
@@ -4,6 +4,8 @@ library(DBI)
 library(stringr)
 
 
+
+
 machineName <- as.character(Sys.info()['nodename'])
 
 if(machineName=='soils-discovery'){
@@ -25,13 +27,13 @@ doQueryFromFed <- function(sql){
 
 makeRandomString <- function(n=1)
 {
-    lenght = sample(c(10:20))
-    randomString <- c(1:n)
-    for (i in 1:n)
-    {
-        randomString[i] <- paste(sample(c(0:9, letters, LETTERS),lenght, replace=TRUE),collapse="")
-    }
-    return(randomString)
+  lenght = sample(c(10:20))
+  randomString <- c(1:n)
+  for (i in 1:n)
+  {
+    randomString[i] <- paste(sample(c(0:9, letters, LETTERS),lenght, replace=TRUE),collapse="")
+  }
+  return(randomString)
 }
 
 isValidEmail <- function(x) {
@@ -51,7 +53,7 @@ addUser <- function(email, firstName, lastName, organisation){
   user <- doQueryFromFed(paste0("Select * from AuthUsers where usrID = '", email, "';"))
 
   if(nrow(user) > 0 ){
-    msg <- paste0("The email address already has an associated API Key.<br> Please contact ross.searle@csiro.au to sort this out.")
+    msg <- paste0("The email address already has an associated API Key.<br> Please contact <a href='mailto:ross.searle@csiro.au?subject=Trouble%20Registering%20for%20the%20SoilDataFederator'> ross.searle@csiro.au</a> to sort this out.")
   }else{
 
     apiKey <- makeRandomString(1)
@@ -89,10 +91,10 @@ insertUser <- function(sqlInsert){
     dbDisconnect(authcon)
     return(T)
 
-   }, error = function(err) {
-     print(err)
-     return(F)
-   })
+  }, error = function(err) {
+    print(err)
+    return(F)
+  })
 }
 
 
@@ -101,109 +103,107 @@ insertUser <- function(sqlInsert){
 ui <- fluidPage(
 
 
-   # tags$head(
-   #     includeCSS("http://esoil.io/TERNLandscapes/SoilDataFederatoR/help/Templates/styles/stylesV6.css")),
-
-
-  includeHTML( paste0(rootDir,'/Docs/TERNLandscapesHeader.html')),
-
-   tags$div(class="container",
-        # tags$div( class="header",
-        #     tags$div(class="headertext1", "Soil and Landscape Grid"),
-        #     tags$div(class="headertext2", "of Australia")),
+  tags$head(
+    #includeCSS("http://esoil.io/TERNLandscapes/SoilDataFederatoR/help/Templates/styles/stylesV6.css")
+    includeHTML( paste0(rootDir,'/Docs/TERNLandscapesHeadNoTags.html'))
+    ),
 
 
 
-        tags$div(id="content",
+  includeHTML( paste0(rootDir,'/Docs/TERNLandscapesTopBodyNoTags.html')),
 
-                # div( HTML("<H1>SoilDataFederator</H1>")),
+  tags$div(class="container", HTML("<br>"),
+           # tags$div( class="header",
+           #           tags$div(class="headertext1", "Soil and Landscape Grid"),
+           #           tags$div(class="headertext2", "of Australia")
+           #           ),
 
-            uiOutput("P1"),
-            br(),
-            tags$table(align="left", width="400px",
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Email Address")),
-                      tags$td(width = "30%", textInput(inputId = "txtEmail", label = NULL, width = 200))),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "First Name")),
-                      tags$td(width = "30%", textInput(inputId = "txtFirstName", label = NULL, width = 200))),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Last Name")),
-                      tags$td(width = "30%", textInput(inputId = "txtLastName", label = NULL, width = 200))),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Organisation")),
-                      tags$td(width = "30%", textInput(inputId = "txtOrganisation", label = NULL, width = 200))),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", HTML("<P></P>")),
-                      tags$td(width = "30%", "")),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", ""),
-                      tags$td(width = "30%", actionButton("doSubmit", "Generate API Key"))),
-              tags$tr(width = "60%",
-                      tags$td(width = "30%", HTML("<P>&nbsp</P>")),
-                      tags$td(width = "30%", HTML("&nbsp"))),
-              tags$tr(width = "60%",
-                      #tags$td(width = "30%", ""),
-                      tags$td(width = "30%", colspan="2", HTML("<P>&nbsp"), div(style = "font-size:14px; font-weight:bold; color:#ebb72f; text-align: center;", uiOutput("SubmitMessage"),  HTML("</P>"))))
-              ),
+           tags$div(id="content",
 
-            br(),br(),br(),
+                    uiOutput("P1"),
+                    br(),
+                    tags$table(align="left", width="300px",
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Email Address")),
+                                       tags$td(width = "30%", textInput(inputId = "txtEmail", label = NULL, width = 200))),
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "First Name")),
+                                       tags$td(width = "30%", textInput(inputId = "txtFirstName", label = NULL, width = 200))),
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Last Name")),
+                                       tags$td(width = "30%", textInput(inputId = "txtLastName", label = NULL, width = 200))),
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", div(style = "font-size:14px; font-weight:bold; text-align: right;", "Organisation")),
+                                       tags$td(width = "30%", textInput(inputId = "txtOrganisation", label = NULL, width = 200))),
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", HTML("")),
+                                       tags$td(width = "30%", "")),
+                               tags$tr(width = "60%",
+                                       tags$td(width = "30%", ""),
+                                       tags$td(width = "30%", actionButton("doSubmit", "Generate API Key"))),
+                               tags$tr(width = "60%",
+                                       #tags$td(width = "30%", ""),
+                                       tags$td(width = "30%", colspan="2", HTML(""), div(style = "font-size:14px; font-weight:bold; color:#ebb72f; text-align: center;", uiOutput("SubmitMessage"),  HTML(""))))
+                    ),
 
-            uiOutput("P2"),
+                    br(),br(),br(),
 
-            includeHTML(paste0(rootDir, '/Docs/TERNLandscapesFooter.html'))
-        )
-   )
+                    uiOutput("P2"),
+
+                    includeHTML(paste0(rootDir, '/Docs/TERNLandscapesBottomBodyNoTags.html'))
+           )
+  )
 
 
 )
 
 server <- function(session, input, output) {
 
-RV <- reactiveValues()
-RV$message <- NULL
+  RV <- reactiveValues()
+  RV$message <- NULL
 
-    subMsg <- eventReactive(input$doSubmit, {
+  subMsg <- eventReactive(input$doSubmit, {
 
-      print('9999999999999999999999999999999999999999999999')
-      msg <- ''
-      if(!isValidEmail(input$txtEmail)){
-          msg <- "The email you supplied does not seem to be valid."
-      }else if(!isValidString(input$txtFirstName)){
-          msg <- "The supplied first name does not seem to be valid."
-      }else if(!isValidString(input$txtLastName)){
-          msg <- "The supplied last name does not appear to be valid."
-      }else if(!isValidString(input$txtOrganisation)){
-          msg <- "The supplied organisation name does not appear to be valid."
-      }else{
-        msg <- addUser(input$txtEmail, input$txtFirstName, input$txtLastName, input$txtOrganisation)
-      }
+    msg <- ''
+    if(!isValidEmail(input$txtEmail)){
+      msg <- "The email you supplied does not seem to be valid."
+    }else if(!isValidString(input$txtFirstName)){
+      msg <- "The supplied first name does not seem to be valid."
+    }else if(!isValidString(input$txtLastName)){
+      msg <- "The supplied last name does not appear to be valid."
+    }else if(!isValidString(input$txtOrganisation)){
+      msg <- "The supplied organisation name does not appear to be valid."
+    }else{
 
-      return(msg)
-    })
+      msg <- addUser(input$txtEmail, input$txtFirstName, input$txtLastName, input$txtOrganisation)
 
+    }
 
-        output$SubmitMessage <- renderText({
-          paste0(subMsg())
-
-          })
+    return(msg)
+  })
 
 
+  output$SubmitMessage <- renderText({
+    paste0(subMsg())
+
+  })
 
 
-    output$P1 <- renderText({
 
-        paste0( '<br><H1>SoilDataFederator API Registration</H1>
+
+  output$P1 <- renderText({
+
+    paste0( '<H1>SoilDataFederator API Registration</H1>
        <p> </p>
-        <p>Register with the SoilDataFederator to obtain an API Key to access the soil data. We require this just so we can update you of changes to the system etc. We may contact you once in a while to ask about how you are using the system. </p>
+        <p>Register with the SoilFederator to obtain an API Key to access the soil data. We require this just so we can update you of changes to the system etc. We may contact you once in a while to ask about how you are using the system. </p>
         <p>Fill in your details below, click submit and you will be sent a registration confirmation email to the address supplied.</p>
 ')
-    })
+  })
 
-    output$P2 <- renderText({
+  output$P2 <- renderText({
 
-      paste0('<br><br>')
-    })
+    paste0('')
+  })
 }
 
 shinyApp(ui = ui, server = server)
