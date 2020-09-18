8fb5ab911de023965134a947005ebe0d2a37e632
Searle <sea084@csiro.au>
2020-09-18 10:06:34 +1000

    TERN Theming

--- Pages/RegisterConfirm/app.R
+++ Pages/RegisterConfirm/app.R
@@ -4,15 +4,18 @@ library(DBI)
 library(stringr)
 library(shinyalert)
 
-# https://shiny.esoil.io/SoilDataFederator/RegisterConfirm/?&email=ross.searle@gmail.com&firstname=Georgia&lastname=Searle&organisation=Brisbane&tkey=2jpft6WvquTLjwO9XqFD
 
 machineName <- as.character(Sys.info()['nodename'])
 
 if(machineName=='soils-discovery'){
-  dbPathSoilsFed <- '/srv/plumber/TERNLandscapes/SoilDataFederatoR/DB/soilsFederator.sqlite'
+  rootDir <- '/srv/plumber/TERNLandscapes/SoilDataFederatoR'
+  dbPathSoilsFed <- paste0(rootDir, '/DB/soilsFederator.sqlite')
 }else{
-  dbPathSoilsFed <- 'C:/Users/sea084/Dropbox/RossRCode/Git/TernLandscapes/APIs/SoilDataFederatoR/DB/soilsFederator.sqlite'
+  rootDir <- 'C:/Users/sea084/Dropbox/RossRCode/Git/TernLandscapes/APIs/SoilDataFederatoR'
+  dbPathSoilsFed <- paste0(rootDir, '/DB/soilsFederator.sqlite')
 }
+
+
 
 doQueryFromFed <- function(sql){
   conn <- DBI::dbConnect(RSQLite::SQLite(), dbPathSoilsFed)
@@ -94,13 +97,18 @@ updateKey <- function(sqlUpdate){
 ui <- fluidPage(
 
   useShinyalert(),
+
   tags$head(
-    includeCSS("http://esoil.io/TERNLandscapes/SoilDataFederatoR/help/Templates/styles/stylesV6.css")),
+    includeHTML( paste0(rootDir,'/Docs/TERNLandscapesHeadNoTags.html'))
+  ),
+
+  includeHTML( paste0(rootDir,'/Docs/TERNLandscapesTopBodyNoTags.html')),
 
   tags$div(class="container",
            tags$div( class="header",
-                     tags$div(class="headertext1", "Soil and Landscape Grid"),
-                     tags$div(class="headertext2", "of Australia")),
+                     # tags$div(class="headertext1", "Soil and Landscape Grid"),
+                     # tags$div(class="headertext2", "of Australia")
+                     ),
 
            tags$div(id="content",
 
@@ -135,7 +143,9 @@ ui <- fluidPage(
 
 
 
-                    uiOutput("P2")
+                    uiOutput("P2"),
+
+                    includeHTML(paste0(rootDir, '/Docs/TERNLandscapesBottomBodyNoTags.html'))
            )
   )
 
@@ -203,18 +213,20 @@ output$APIKeyMessage <- renderText({
 
   output$P2 <- renderText({
 
-    paste0('<br><br> <br><br><p></p>
-        <div id="footer">
-        <h1 class="textinbar">SLGA Funders<span style="margin-left: 22%;">Partners</span></h1>
-        <div id="partners"> <a href="http://www.ga.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/AustGovtGSA_stack.gif" alt="Australian Government Geosciences Australia logo" height="59" width="90"></a> <a href="http://www.csiro.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/CSIRO.gif" alt="CSIRO logo" height="54" width="55"></a> <a href="http://www.environment.nsw.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NSWOEH.gif" alt="NSW Office of Environment and Heritage logo" height="44" width="120"></a> <a href="http://www.lrm.nt.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NTGovt.gif" alt="Northern Territory Government logo" height="55" width="59"></a> <a href="http://www.qld.gov.au/dsitia/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/QLDGovt.gif" alt="Queensland Government logo" height="69" width="55"></a> <a href="http://www.environment.sa.gov.au/Home" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/SAGovt_DEWNR.gif" alt="South Australian Department of Environment, Water and Natural Resources logo" height="67" width="85"></a> <a href="http://dpipwe.tas.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/TasGov.png" style="margin-right: 0%;" alt="Tasmanian Government logo" height="56" width="68"></a> <a href="http://sydney.edu.au/agriculture/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/Uni-of-Sydney.gif" alt="University of Sydney logo" height="44" width="112"></a> <a href="http://www.depi.vic.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/VICGovt_DEPI.gif" alt="Victorian Government Department of Environment and Primary Industries logo" height="32" width="137"></a> <a href="https://www.agric.wa.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/WAGovt_DAF.gif" alt="Western Australian Government Department of Agriculture and Food logo" height="38" width="168"></a> <a href="http://www.clw.csiro.au/aclep/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/ACLEP.gif" alt="ACLEP logo" height="44" width="165"></a> </div>
-        </div>
-        <div id="funder">
-        <p> <a href="http://www.tern.org.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/TERNLogo.gif" alt="TERN logo" height="83" width="90"></a><a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NCRIS.gif" style="margin-left: 8%;" alt="NCRIS logo" height="62" width="130"></a></p>
-        <p>TERN is supported by the Australian Government through the National
-        Collaborative Research Infrastructure Strategy and the Super Science
-        Initiative.</p>
-        </div>
-        </div>')
+    paste0('<br><br>')
+
+    # <br><br><p></p>
+    #     <div id="footer">
+    #     <h1 class="textinbar">SLGA Funders<span style="margin-left: 22%;">Partners</span></h1>
+    #     <div id="partners"> <a href="http://www.ga.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/AustGovtGSA_stack.gif" alt="Australian Government Geosciences Australia logo" height="59" width="90"></a> <a href="http://www.csiro.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/CSIRO.gif" alt="CSIRO logo" height="54" width="55"></a> <a href="http://www.environment.nsw.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NSWOEH.gif" alt="NSW Office of Environment and Heritage logo" height="44" width="120"></a> <a href="http://www.lrm.nt.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NTGovt.gif" alt="Northern Territory Government logo" height="55" width="59"></a> <a href="http://www.qld.gov.au/dsitia/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/QLDGovt.gif" alt="Queensland Government logo" height="69" width="55"></a> <a href="http://www.environment.sa.gov.au/Home" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/SAGovt_DEWNR.gif" alt="South Australian Department of Environment, Water and Natural Resources logo" height="67" width="85"></a> <a href="http://dpipwe.tas.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/TasGov.png" style="margin-right: 0%;" alt="Tasmanian Government logo" height="56" width="68"></a> <a href="http://sydney.edu.au/agriculture/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/Uni-of-Sydney.gif" alt="University of Sydney logo" height="44" width="112"></a> <a href="http://www.depi.vic.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/VICGovt_DEPI.gif" alt="Victorian Government Department of Environment and Primary Industries logo" height="32" width="137"></a> <a href="https://www.agric.wa.gov.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/WAGovt_DAF.gif" alt="Western Australian Government Department of Agriculture and Food logo" height="38" width="168"></a> <a href="http://www.clw.csiro.au/aclep/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/ACLEP.gif" alt="ACLEP logo" height="44" width="165"></a> </div>
+    #     </div>
+    #     <div id="funder">
+    #     <p> <a href="http://www.tern.org.au/" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/TERNLogo.gif" alt="TERN logo" height="83" width="90"></a><a href="https://education.gov.au/national-collaborative-research-infrastructure-strategy-ncris" target="_blank"><img src="http://www.clw.csiro.au/aclep/soilandlandscapegrid/logos/NCRIS.gif" style="margin-left: 8%;" alt="NCRIS logo" height="62" width="130"></a></p>
+    #     <p>TERN is supported by the Australian Government through the National
+    #     Collaborative Research Infrastructure Strategy and the Super Science
+    #     Initiative.</p>
+    #     </div>
+    #     </div>')
   })
 }
 
