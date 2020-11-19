1bd811c737162e906ec5c10d9d34b4dfc7447bf0
Ross Searle <sea084@soils-discovery.it.csiro.au>
2020-11-19 15:48:58 +1100

    sync

--- Backends.R
+++ Backends.R
@@ -335,7 +335,7 @@ generateResponseDF <- function( dataset, location_id, layer_id, sample_id, date,
                       Longitude=longitude, Latitude=latitude ,
                       UpperDepth=upperDepth, LowerDepth=lowerDepth, PropertyType=dataType, ObservedProperty=observedProp,
                       Value=value , Units=units, QualCollection=NA, QualSpatialAggregation=NA, QualManagement=NA, QualSpatialAccuracy=NA, stringsAsFactors = F)
-  oOutDF <- outDF[order(outDF$Location_ID, outDF$Dataset, outDF$UpperDepth, outDF$SampleID),]
+  oOutDF <- outDF[order(outDF$Dataset, outDF$Location_ID, outDF$UpperDepth, outDF$LowerDepth)]
 
   return(oOutDF)
 }
