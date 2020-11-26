97e50a100840e258c907c6d79917dc106c314f2a
Searle <sea084@csiro.au>
2020-11-27 08:43:14 +1000

    sync

--- Testing/runFunctionTests.R
+++ Testing/runFunctionTests.R
@@ -3,25 +3,25 @@ library(stringr)
 source('C:/Users/sea084/Dropbox/RossRCode/Git/TernLandscapes/APIs/SoilDataFederatoR/Backends.R')
 
 
-test_Functions <- function(types='ALL', showResults=T, nrows=5){
+test_Functions <- function(types='ALL', showResults=T, nrows=5, API=F){
 
   props <- read.csv('C:/Users/sea084/Dropbox/RossRCode/Git/TernLandscapes/APIs/SoilDataFederatoR/Testing/testProps.csv')
 
   if(str_to_upper(types)=='ALL'){
-    test_getLabData_Functions(props, showResults, nrows)
-    test_getMorphData_Functions(props, showResults, nrows)
-    test_getLocations_Functions(props, showResults, nrows)
+    test_getLabData_Functions(props, showResults, nrows, API)
+    test_getMorphData_Functions(props, showResults, nrows, API)
+    test_getLocations_Functions(props, showResults, nrows, API)
   }  else if(str_to_upper(types)=='LAB'){
-    test_getLabData_Functions(props, showResults, nrows)
+    test_getLabData_Functions(props, showResults, nrows, API)
   }  else if(str_to_upper(types)=='MORPH'){
-    test_getMorphData_Functions(props, showResults, nrows)
+    test_getMorphData_Functions(props, showResults, nrows, API)
   }else if(str_to_upper(types)=='LOCATIONS'){
-    test_getLocations_Functions(props, showResults, nrows)
+    test_getLocations_Functions(props, showResults, nrows, API)
   }
 }
 
 
-test_getMorphData_Functions <- function(props, showResults, nrows){
+test_getMorphData_Functions <- function(props, showResults, nrows, API=F){
 
      cat(crayon::yellow('\n\nTesting Morphology Data EndPoints', sep=''))
      cat(crayon::yellow('\n=====================================', sep=''))
@@ -39,7 +39,14 @@ test_getMorphData_Functions <- function(props, showResults, nrows){
       cat(crayon::magenta('No props available', sep=''))
     }else{
 
-      r1 <- getSoilData(DataSets=dSt,observedProperty=OP, usr='ross.searle@csiro.au', key='a', verbose=F)
+      if(API){
+        url <- paste0("http://esoil.io/TERNLandscapes/SoilDataFederatoR/SoilDataAPI/SoilData?observedProperty=", OP,"&DataSet=",dSt, "&usr=ross.searle@csiro.au&key=a")
+        print(url)
+        r1 <- fromJSON(url)
+      }else{
+        r1 <- getSoilData(DataSets=dSt,observedProperty=OP, usr='ross.searle@csiro.au', key='a', verbose=F)
+      }
+
 
       if(nrow(r1) > 0){
         cat(crayon::green('SUCCESS', sep=''))
@@ -54,7 +61,7 @@ test_getMorphData_Functions <- function(props, showResults, nrows){
 }
 
 
-test_getLabData_Functions <- function(props, showResults, nrows){
+test_getLabData_Functions <- function(props, showResults, nrows, API){
 
      cat(crayon::yellow('\n\nTesting Lab Data EndPoints', sep=''))
      cat(crayon::yellow('\n=====================================', sep=''))
@@ -74,7 +81,14 @@ test_getLabData_Functions <- function(props, showResults, nrows){
       cat(crayon::magenta('No props available', sep=''))
     }else{
 
-      r1 <- getSoilData(DataSets=dSt,observedProperty=OP, usr='ross.searle@csiro.au', key='a', verbose=F)
+      if(API){
+        url <- paste0("http://esoil.io/TERNLandscapes/SoilDataFederatoR/SoilDataAPI/SoilData?observedProperty=", OP,"&DataSet=",dSt, "&usr=ross.searle@csiro.au&key=a")
+        print(url)
+        r1 <- fromJSON(url)
+      }else{
+        r1 <- getSoilData(DataSets=dSt,observedProperty=OP, usr='ross.searle@csiro.au', key='a', verbose=F)
+      }
+
 
       if(nrow(r1) > 0){
         cat(crayon::green('SUCCESS', sep=''))
@@ -89,7 +103,7 @@ test_getLabData_Functions <- function(props, showResults, nrows){
 }
 
 ####  Test the getLocations EndPoints
-test_getLocations_Functions <- function(props, showResults, nrows){
+test_getLocations_Functions <- function(props, showResults, nrows, API){
 
   ds = props$DataSet
 
@@ -101,7 +115,16 @@ test_getLocations_Functions <- function(props, showResults, nrows){
       cat(crayon::blue('\n', dSt,' : ', sep=''))
 
       tryCatch({
-            r2 <- getSiteLocations(DataSets=dSt, usr='ross.searle@csiro.au', key='a')
+
+        if(API){
+          url <- paste0("http://esoil.io/TERNLandscapes/SoilDataFederatoR/SoilDataAPI/ObservationLocations?DataSet=",dSt, "&usr=ross.searle@csiro.au&key=a")
+          print(url)
+          r1 <- fromJSON(url)
+        }else{
+          r2 <- getSiteLocations(DataSets=dSt, usr='ross.searle@csiro.au', key='a')
+        }
+
+
 
             if(nrow(r2) > 0){
               cat(crayon::green('SUCCESS', sep=''))
